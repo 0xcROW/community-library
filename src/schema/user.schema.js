b26d65e4a52be7dcd1377084de1c3edab67d01e9
Matheus Silva <bastiaotreze@gmail.com>
2024-10-31 01:59:35 -0300

    feat: schema with zod

---
+++ src/schema/user.schema.js
@@ -0,0 +1,10 @@
+import {z} from 'zod'
+
+const userSchema = z.object({
+    username: z.string().min(3, 'User name is required!'),
+    email: z.string.email('Invalid email address!'),
+    password: z.string.min(6, 'Password must have at least 6 characters'),
+    avatar: z.string.url('Invalid URL').optional()
+})
+
+export {userSchema}
