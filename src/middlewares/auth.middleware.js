72f3be6dd9c211e80d2688dfc55a7239cc188398
Matheus Silva <bastiaotreze@gmail.com>
2024-11-06 00:15:06 -0300

    hotfix: messages

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,12 +4,12 @@ import userService from "../service/user.services.js";
 
 export function authMiddleware(req, res, next) {
   const tokenHeader = req.headers.authorization; // Get token from header
-  if (!tokenHeader) return res.status(401).send("Access Denied"); // If no token, return 401
+  if (!tokenHeader) return res.status(401).send("No token informed, access denied"); // If no token, return 401
 
   const tokenSplit = tokenHeader.split(" "); // Split the token into an array
-  if (tokenSplit.length !== 2) return res.status(401).send("Access Denied"); // If token is not in the format 'Bearer token', return 401
+  if (tokenSplit.length !== 2) return res.status(401).send("Invalid token, access denied"); // If token is not in the format 'Bearer token', return 401
 
-  if (!/^Bearer$/i.test(tokenSplit[0])) return res.status(401).send("Access Denied, malformatted token"); // If no regex match on the first position, return 401
+  if (!/^Bearer$/i.test(tokenSplit[0])) return res.status(401).send("Malformatted token, access denied"); // If no regex match on the first position, return 401
 
   jwt.verify(tokenSplit[1], process.env.JWT_SECRET, async (err, decoded) => {
     if (err) {
